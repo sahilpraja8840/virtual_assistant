5ada130388eb6a0ecb9edf6c2614441b64225f3e
Sahil Prajapati <sahilp8840@gmail.com>
2025-05-28 12:11:50 +0530

    Delete new.js

--- new.js
+++
@@ -1,8 +0,0 @@
-const micBtn = document.getElementById('mic-btn');
-const micIcon = document.getElementById('mic-icon');
-let isListening = false;
-
-micBtn.addEventListener('click', () => {
-  isListening = !isListening;
-  micIcon.className = isListening ? 'fa-solid fa-microphone-lines-slash' : 'fa-solid fa-microphone-lines';
-});
